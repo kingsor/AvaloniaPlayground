dc40b0bcd6cedb204a07577f8f142455c413a09d
Edoardo Inamo <avventureweb@gmail.com>
2026-08-28 15:57:50 +0200

    Add XPlat Application

---
+++ src/XplatApp/XplatApp/Views/MainView.axaml.cs
@@ -0,0 +1,13 @@
+
+using Avalonia;
+using Avalonia.Controls;
+
+namespace XplatApp.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
